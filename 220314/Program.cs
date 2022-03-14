87df9f7d63ad019c671a1d4248d777a2f2cc43f1
user <user@DESKTOP-6JBEF5K>
2022-03-14 10:01:47 +0800

    OK

---
+++ 220314/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _220314
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            Console.WriteLine("輸入數字");
+            int num = Convert.ToInt32(Console.ReadLine());
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("是偶數");
+            }
+            else
+            {
+                Console.WriteLine("是奇數");
+            }
+        }
+    }
+}
