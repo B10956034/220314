f171cc79d3a0f31565533523d7ab6d49f354203c
user <user@DESKTOP-6JBEF5K>
2022-03-14 10:20:15 +0800

    test

--- 220314/Program.cs
+++ 220314/Program.cs
@@ -10,14 +10,23 @@ namespace _220314
 
             Console.WriteLine("輸入數字");
             int num = Convert.ToInt32(Console.ReadLine());
-            if (num % 2 == 0)
+            if (num % 2 != 0)
             {
-                Console.WriteLine("是偶數");
+                Console.WriteLine("Weird");
+            }
+            else if(num >=2 && num <= 5)
+            {
+                Console.WriteLine("Not weird");
+            }
+            else if(num >=6 && num <= 20)
+            {
+                Console.WriteLine("Weird");
             }
             else
             {
-                Console.WriteLine("是奇數");
+                Console.WriteLine("Not Weird");
             }
+            
         }
     }
 }
